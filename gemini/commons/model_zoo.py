a6c1e646df87bcdc3beb400445893e8822e7fda5
MEllis <21113023+MEllis-github@users.noreply.github.com>
2023-03-29 17:06:09 -0400

    add model configs for 40b, 50b, ... 68b

--- gemini/commons/model_zoo.py
+++ gemini/commons/model_zoo.py
@@ -53,6 +53,29 @@ def gpt2_24b(checkpoint=True):
     return GPTLMModel(hidden_size=8192, num_layers=30, num_attention_heads=16, checkpoint=checkpoint)
 
 
+def gpt2_30b(checkpoint=True):
+    return GPTLMModel(hidden_size=8192, num_layers=37, num_attention_heads=16, checkpoint=checkpoint)
+
+
+def gpt2_40b(checkpoint=True):
+    return GPTLMModel(hidden_size=8192, num_layers=50, num_attention_heads=16, checkpoint=checkpoint)
+
+
+# https://github.com/Tencent/PatrickStar/blob/0539e41e2b253124c2a6fca6e0fee3f4ba020c5c/examples/model_builder.py#L158-L162
+def gpt2_50b(checkpoint=True):
+    return GPTLMModel(hidden_size=8192, num_layers=62, num_attention_heads=16, checkpoint=checkpoint)
+
+
+# https://github.com/Tencent/PatrickStar/blob/0539e41e2b253124c2a6fca6e0fee3f4ba020c5c/examples/model_builder.py#L163-L167
+def gpt2_60b(checkpoint=True):
+    return GPTLMModel(hidden_size=8192, num_layers=75, num_attention_heads=16, checkpoint=checkpoint)
+
+
+# https://github.com/Tencent/PatrickStar/blob/0539e41e2b253124c2a6fca6e0fee3f4ba020c5c/examples/model_builder.py#L168-L172
+def gpt2_68b(checkpoint=True):
+    return GPTLMModel(hidden_size=9216, num_layers=66, num_attention_heads=16, checkpoint=checkpoint)
+
+
 def model_builder(model_size: str) -> callable:
     if model_size == "gpt2_medium":
         return gpt2_medium
@@ -66,6 +89,16 @@ def model_builder(model_size: str) -> callable:
         return gpt2_20b
     elif model_size == "gpt2_24b":
         return gpt2_24b
+    elif model_size == "gpt2_30b":
+        return gpt2_30b
+    elif model_size == "gpt2_40b":
+        return gpt2_40b
+    elif model_size == "gpt2_50b":
+        return gpt2_50b
+    elif model_size == "gpt2_60b":
+        return gpt2_60b
+    elif model_size == "gpt2_68b":
+        return gpt2_68b
     else:
         raise TypeError(f"model_builder {model_size}")
 
